bcb0d1d15479dfff40c6bf0163e7fd83df217b3d
Isaak D <isaakddyck@hotmail.com>
2019-12-12 19:15:38 -0600

    Updates

---
+++ precache-manifest.bed623fb7f07f1ee9cb1d79b774b9a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8404e322ab961fb19ceac518c8e657",
+    "url": "/comit-final-project/index.html"
+  },
+  {
+    "revision": "5ffc2407334adb90712b",
+    "url": "/comit-final-project/static/css/main.7d627790.chunk.css"
+  },
+  {
+    "revision": "d0475efc8ad6ada138cf",
+    "url": "/comit-final-project/static/js/2.b648675b.chunk.js"
+  },
+  {
+    "revision": "5ffc2407334adb90712b",
+    "url": "/comit-final-project/static/js/main.8abec078.chunk.js"
+  },
+  {
+    "revision": "6520881554d5dfcb1bd0",
+    "url": "/comit-final-project/static/js/runtime-main.4d3e32c2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/comit-final-project/static/media/logo.25bf045c.svg"
+  }
+]);
