a309160951d42dc0d0db69189e8fd16d61f5627e
Isaak D <isaakddyck@hotmail.com>
2019-12-12 19:11:46 -0600

    Updates

---
+++ precache-manifest.bece14e4b3f4494ba2abbfd64eeb43f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c830460e1dad3d05a8ce976d8c697c17",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "4d50b004debd08af760c",
+    "url": "/react-project/static/css/main.7d627790.chunk.css"
+  },
+  {
+    "revision": "d0475efc8ad6ada138cf",
+    "url": "/react-project/static/js/2.b648675b.chunk.js"
+  },
+  {
+    "revision": "4d50b004debd08af760c",
+    "url": "/react-project/static/js/main.2742733f.chunk.js"
+  },
+  {
+    "revision": "efea1498724c15553664",
+    "url": "/react-project/static/js/runtime-main.41227bcd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-project/static/media/logo.25bf045c.svg"
+  }
+]);
